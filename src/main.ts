0462e6dbd929f31d7c8aec23b19f8005817a5a7d
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-07-28 22:55:29 +0500

    [FEAT] Added global prefix 🤪

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import * as compression from 'compression';
 import helmet from 'helmet';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+
+  app.setGlobalPrefix('v1');
+
+  app.enableShutdownHooks();
 
   app.enableCors({
     origin: ['http://localhost:3001'],
@@ -16,6 +21,9 @@ async function bootstrap() {
   // TODO: Further configure these middlewares
   app.use(compression());
   app.use(helmet());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(3000);
 }
